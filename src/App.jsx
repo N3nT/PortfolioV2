43ebe245ef46d3fc7c1cddfd6aa192f704049962
N3nT <bar55v71@interia.pl>
2025-02-14 12:15:08 +0100

    Hello section - mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import Nav from './components/Nav';
+import Hello from './components/Hello';
 
-function App() {
+function App() {  
   return (
-    <>
-      <p>Hellow World</p>
-    </>
+    <div className="bg-radial-[at_40%_40%] from-stone-900 to-stone-950 to-70% h-screen overflow-auto relative z-20 font-[Red_Hat_Display]">
+      <Nav/>
+      <Hello/>
+      <div className='h-[1000px]'>
+        <p className='text-white'>dupa</p>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+const Underline = () => {
+    return(
+        <div className="w-full h-[1px] bg-white animate-underline"></div>
+    )
+}
+
+const Nav = () => {
+    const [activeSection, setActiveSection] = useState(0);
+
+    return(
+        <nav className="backdrop-blur-xs sticky top-0 w-full flex justify-center border-b-[1px] border-stone-400 z-10">
+            <div className="max-w-[1000px]">
+                <ul className="text-white flex w-full h-[60px]">
+                    <li className="p-4 text-lg flex-col">
+                        <p>Home</p>
+                        {activeSection === 0 ? <Underline/> : null}
+                    </li>
+                    <li className="p-4 text-lg md:mx-5">
+                        <p>Projekty</p>
+                        {activeSection === 1 ? <Underline/> : null}
+                    </li>
+                    <li className="p-4 text-lg">
+                        <p>Kontakt</p>
+                        {activeSection === 2 ? <Underline/> : null}
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
