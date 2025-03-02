917f9d1470bd0d4f2573126accbdcbb55aee12d4
N3nT <bar55v71@interia.pl>
2025-03-02 21:27:26 +0100

    Translate english words

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -51,7 +51,7 @@ const Form = () => {
         <input type="email" name="user_email" className='bg-white p-2 mb-5' placeholder='Twój email' onChange={(e) => {handleInputsChange(setEmail, e)}} value={email} required/>
         <label className='text-white text-lg md:text-xl'>Wiadomość</label>
         <textarea name="message" className='bg-white p-2' placeholder='Treść wiadomości' rows={5} onChange={(e) => {handleInputsChange(setMessage, e)}} value={message} required/>
-        <input type="submit" value="Send" className='bg-green-400 hover:bg-green-700 text-white p-5 cursor-pointer transition-colors'/>
+        <input type="submit" value="Wyślij" className='bg-green-400 hover:bg-green-700 text-white p-5 cursor-pointer transition-colors'/>
         {successStatus ? 
         (<div className='fixed top-10 -translate-x-1/2 left-1/2 bg-stone-950 w-80 h-20 z-10 border-4 border-green-400 rounded-xl text-center flex items-center justify-center flex-col overflow-hidden p-4'>
             <p className='text-white text-xl'>Wiadomość wysłana pomyślnie!</p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,13 +16,13 @@ const Projects = ({setProjectsActive}) => {
     return(
         <section className="flex flex-col items-center mb-15 scroll-mt-[100px]" ref={ref} id='projects'>
             <h2 className="text-white text-center text-3xl sm:text-4xl">Projekty</h2>
-            <h3 className='text-white/70 text-xl mt-10 sm:text-2xl'>WebDevelopment</h3>
+            <h3 className='text-white/70 text-xl mt-10 sm:text-2xl'>Aplikacje internetowe</h3>
             <div className='flex flex-col md:flex-row items-center *:md:mx-5 max-w-[1100px] flex-wrap justify-center'>
                 {webProjectsData ? webProjectsData.projects.map((project, index) => {
                 return(<WebProjectCard key={index} projectImg={project.image} techStack={project.techstack} liveLink={project.liveLink} repoLink={project.repoLink} projectTitle={project.name}/>)
                 }) : null}
             </div>
-            <h3 className='text-white/70 text-xl my-10 sm:text-2xl'>Other</h3>
+            <h3 className='text-white/70 text-xl my-10 sm:text-2xl'>Inne</h3>
             <div className='flex flex-wrap justify-center items-center max-w-[1000px]'>
                {otherProjectsData ? (otherProjectsData.projects.map((project, index) => {
                     return (<OtherProjectCard key={index} techStack={project.techstack} title={project.name} link={project.repoLink}/>)
@@ -30,7 +30,7 @@ const Projects = ({setProjectsActive}) => {
                : 
                (null)}
             </div>
-            <a href="https://github.com/N3nT?tab=repositories" className='bg-green-400 text-white p-5 text-lg rounded-lg mt-10 hover:bg-green-700 w-[200px] text-center transition-colors' aria-label="link do wszystkich repozytorium" target="_blank">Check all projects</a>
+            <a href="https://github.com/N3nT?tab=repositories" className='bg-green-400 text-white p-5 text-lg rounded-lg mt-10 hover:bg-green-700 w-[200px] text-center transition-colors' aria-label="link do wszystkich repozytorium" target="_blank">Wszystkie projekty</a>
         </section>
     )
 }
